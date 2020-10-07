bc0b23e7dc6ed3ee5be3c2318233bb7c6151797d
Jonah Rowlinson <jonah@rowlinson.eu>
2020-10-07 20:37:42 +0100

    Added SASS support
    App now uses SASS for styles. WebStorm automatically compiles on save on
    Kitchen Desktop. Will need to setup and install on Surface.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, console.log("App listening on 3000"));
