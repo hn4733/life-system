a7b92f81939e66d6682ba60379f8151a62d801b2
Jonah Rowlinson <jonah@rowlinson.eu>
2020-10-10 20:33:03 +0100

    Started trying to implement notifications

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {Sequelize, DataTypes} = require('sequelize');
 const sequelize = new Sequelize({dialect: 'sqlite', storage: './db/database.sqlite', logging: false});
+const schedule = require('node-schedule');
+const qs = require('qs');
+const axios = require('axios');
 
 // Test database connection
 try {
@@ -25,7 +28,34 @@ const Todo = require('./db/models/Todo')(sequelize, DataTypes); // Defines the t
 Todo.sync({alter: true}).then(() => console.log('Todo model synced')); // Syncs table to database
 
 const Homework = require('./db/models/Homework')(sequelize, DataTypes); // Defines the table
-Homework.sync({alter: true}).then(() => console.log('Homework model synced')); // Syncs table to database
+Homework.sync({alter: true}).then(() => {
+    console.log('Homework model synced')
+    sendNotification().then(() => console.log('Notifs sent'));
+}); // Syncs table to database
+
+const sendNotification = async () => {
+    let homework = await Homework.findAll();
+    if (homework.length > 0) {
+        for (let i = 0; i < homework.length; i++) {
+            axios({
+                method: "post",
+                url: "",
+                data: qs.stringify({
+                    app_key: "QlEb5FF9JliSHpnW2SzX",
+                    app_secret: "WvsXjK0oE4hD4ACPjSSkGzkIvRtooHjBexWcvl1qdL4yGjyFUFgUuCpIuIGMRb1z",
+                    content: "You have " + homework[i].subject + " to do. Due in " + formatDate(homework[i].due) + "!",
+                    target_type: 'app',
+                    target_alias: 'TheTaskList'
+                }),
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                }
+            });
+        }
+    }
+}
+
+
 
 // Express routes
 app.get('/', (req, res) => {
@@ -50,7 +80,7 @@ app.get('/todo/delete/:id', (req, res) => {
 // Homework routes
 app.get('/homework', async (req, res) => {
     let homework = await Homework.findAll();
-    res.render('homework/index', {homework:homework});
+    res.render('homework/index', {homework: homework});
 });
 app.get('/homework/new', (req, res) => {
     res.render('homework/new');
@@ -85,7 +115,29 @@ app.post('/homework/add', (req, res) => {
 // Test route
 app.get('/test', (req, res) => {
     res.json({message: 'Passed!'});
-})
+});
+
+function formatDate(date) {
+    let today = new Date();
+    let tomorrow = new Date();
+    tomorrow.setDate(today.getDate() + 1);
+    if (date.getDate() == today.getDate() &&
+        date.getMonth() == today.getMonth() &&
+        date.getFullYear() == today.getFullYear()) {
+        return "today";
+    } else if (date.getDate() == tomorrow.getDate() &&
+        date.getMonth() == tomorrow.getMonth() &&
+        date.getFullYear() == tomorrow.getFullYear()) {
+        return "tomorrow";
+    } else {
+        return [date.getDay(), date.getMonth() + 1, date.getFullYear()].join('-');
+    }
+}
+
+var j = schedule.scheduleJob('1 6,18,20 * * *', () => {
+        sendNotification().then(() => console.log('Sent notifications'));
+        return;
+});
 
 // Tell express to listen on port 3000
 module.exports = app;
