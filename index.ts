892089e61ea99fe2f336b6450f198be2dfdb14e3
Jonah Rowlinson <jonah@rowlinson.eu>
2020-10-10 10:25:31 +0100

    Now adds to database
    Currently, /todo doesn't work - need to sort select queries.

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {Sequelize, DataTypes} = require('sequelize');
-const sequelize = new Sequelize({dialect: 'sqlite', storage: './db/database.sqlite', logging:false});
+const sequelize = new Sequelize({dialect: 'sqlite', storage: './db/database.sqlite', logging: false});
 
 // Test database connection
 try {
@@ -17,7 +17,7 @@ const app = express();
 // Express config
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Database Setup
@@ -28,19 +28,23 @@ Todo.sync({alter: true}).then(() => console.log('All models synced')); // Syncs 
 app.get('/', (req, res) => {
     res.render('index');
 });
-app.get('/todo', (req, res) => {
-    res.render('todo/index');
+app.get('/todo', async (req, res) => {
+    let tasks = await Todo.findAll();
+    res.render('todo/index', {tasks:tasks});
 });
-app.get('/todo/new', (req,res) => {
-   res.render('todo/new');
+app.get('/todo/new', (req, res) => {
+    res.render('todo/new');
 });
 // TODO: Add todo:id or todo:slug to routes
 
 // Post requests
 app.post('/todo/add', (req, res) => {
-   console.log(req.body);
-
-   res.redirect('/todo');
+    Todo.create({
+        title: req.body.title,
+        description: req.body.description,
+        due: req.body.due
+    }).then(() => console.log("Added new todo with title \"" + req.body.title + "\""));
+    res.redirect('/todo');
 });
 
 // Tell express to listen on port 3000
